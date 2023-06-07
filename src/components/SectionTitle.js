035da6f10450af1b6e75e79b6573e33d7b596478
Davis <davis-wolfermann2005@gmx.de>
2023-06-07 18:15:53 +0200

    [SectionTitle] Improved beta badge

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -5,23 +5,11 @@ export function SectionTitle({ title, description, beta }) {
         <div className="section-title mb-3">
             <div className="d-flex align-items-center mb-2 flex-column">
                 <div className="d-grid gap-3 d-sm-flex justify-content-sm-center">
-                    <h2 className="mb-0">{title}</h2>
-                    {
-                        beta ?
-                            <>
-                                <div className="d-flex justify-content-center align-items-center">
-                                    <div className="rounded-pill bg-warning p-2 text-black">
-                                        <text>BETA</text>
-                                    </div>
-                                </div>
-                            </>
-                            :
-                            <></>
-                    }
+                    <h2 className="mb-0">{title}{beta ? <span class="badge rounded-pill text-bg-warning ms-3">BETA</span> : ''}</h2>
                 </div>
             </div>
             <div className="line mb-4" />
             <p>{description}</p>
-        </div>
+        </div >
     )
 }
